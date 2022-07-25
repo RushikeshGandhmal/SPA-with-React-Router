f650650f276bd69f712d65529615bf71b0990d56
RushikeshGandhmal <60548902+RushikeshGandhmal@users.noreply.github.com>
2022-07-25 22:50:19 +0530

    Update Products.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -6,7 +6,7 @@ const Products = () => {
       <h1>The Products Page</h1>
       <ul>
         <li>
-          <Link to="/products/p1">A Book </Link>
+          <Link to="/products/p1">A Book</Link>
         </li>
         <li>
           <Link to="/products/p2">A Carpet</Link>
